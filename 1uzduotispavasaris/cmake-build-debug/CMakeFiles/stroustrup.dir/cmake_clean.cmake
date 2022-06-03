b1bbfae0d2b09c7b361ac3d5a529d6473e7a2f8d
U. dulksnyte <95549187+UDulksnyte@users.noreply.github.com>
2022-06-03 17:40:28 +0300

    Add files via upload

---
+++ 1uzduotispavasaris/cmake-build-debug/CMakeFiles/stroustrup.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stroustrup.dir/main.cpp.obj"
+  "CMakeFiles/stroustrup.dir/main.cpp.obj.d"
+  "libstroustrup.dll.a"
+  "stroustrup.exe"
+  "stroustrup.exe.manifest"
+  "stroustrup.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stroustrup.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1uzduotispavasaris/cmake-build-debug/CMakeFiles/stroustrup.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for stroustrup.
+# This may be replaced when dependencies are built.
