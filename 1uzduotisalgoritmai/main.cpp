399e0e2a554ced2110d2dc49e426265ccba5f685
U. dulksnyte <95549187+UDulksnyte@users.noreply.github.com>
2022-06-30 11:40:16 +0300

    Add files via upload

---
+++ 1uzduotisalgoritmai/main.cpp
@@ -0,0 +1,26 @@
+#include "Libraries.h"
+#include "Functions.h"
+
+int main(){
+    vector<string> details;
+    vector<TYPES>firstRow;
+    vector<vector<TYPES>>converted;
+    vector<string>strings;
+    vector<char>columnsNames;
+    bool wrongInput;
+    int x=1;
+
+    string filename="C:\\Users\\37067\\Downloads\\uzduotys-main\\1uzduotisalgoritmai\\cmake-build-debug\\CMakeFiles\\failas.csv";
+    details=readFile(filename,details);
+    getTypes(details,converted);
+    compareData(converted);
+cout<<converted.size()<<endl;
+    for(int d=0;d<firstRow.size()-1;d++){
+
+    }
+    cout<<endl;
+    showDetails(converted,strings);
+
+    return 0;
+
+}
